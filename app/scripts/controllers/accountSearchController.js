2ae843a96b75a239e1c0b0549f5a127f453dded8
mac <luckysevenbear@gmail.com>
2016-12-26 11:55:54 +0200

    changed accountSearchController

--- app/scripts/controllers/accountSearchController.js
+++ app/scripts/controllers/accountSearchController.js
@@ -1,26 +1,26 @@
 'use strict';
 /**
  * @ngdoc function
- * @name Quintet.controller:accountSearchController
+ * @name sbAdminApp.controller:accountSearchController
  * @description
  * # MainCtrl
- * Controller of the Quintet
+ * Controller of the sbAdminApp
  */
-angular.module('Quintet')
-    .controller('accountSearchController', function (mainService, $scope, $stateParams, AccountService) {
+angular.module('sbAdminApp')
+    .controller('accountSearchController', function (cfgService, $scope, $stateParams, AccountService) {
         $scope.accounts = [];
         $scope.loading = false;
         $scope.hasDisplayedSpinner = false;
         $scope.mode = ($stateParams.mode) ? $stateParams.mode : "all";
 
         // Load the last search query
-        $scope.query = (mainService.cache.accountSearchController_last_query) ? mainService.cache.accountSearchController_last_query : "";
+        $scope.query = (cfgService.cache.accountSearchController_last_query) ? cfgService.cache.accountSearchController_last_query : "";
 
         $scope.runQuery = function () {
             console.log("Update is running for query=", $scope.query);
 
             var query = $scope.query.trim();
-            mainService.cache.accountSearchController_last_query = query;
+            cfgService.cache.accountSearchController_last_query = query;
 
             $scope.loading = true;
 
@@ -29,14 +29,24 @@ angular.module('Quintet')
                     $scope.accounts = response;
                 }
                 if ($scope.hasDisplayedSpinner) {
-                    mainService.hideSpinner();
+                    cfgService.hideSpinner();
                     $scope.hasDisplayedSpinner = false;
                 }
                 $scope.loading = false;
             });
         };
 
-        mainService.showSpinner("Loading Accounts...");
+        cfgService.showSpinner("Loading Accounts...");
         $scope.hasDisplayedSpinner = true;
         $scope.runQuery();
+
+        $scope.getFavoriteAccounts = function() {
+            AccountService.getFavoriteAccounts(function(response) {
+                if(response) {
+                    $scope.favorite_accounts = response;
+                }
+            });
+        };
+
+        $scope.getFavoriteAccounts();
     });
