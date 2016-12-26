7db4dac3479786937d2dde950d58684150219d01
bear <mac@Tables-Mac-mini.local>
2016-12-26 10:33:04 +0200

    create skeleton project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quintetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quintetApp
+ */
+angular.module('quintetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
