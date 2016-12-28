39e4c44698e91587cc2616c32f7fcf86868ffa63
mac <luckysevenbear@gmail.com>
2016-12-28 11:58:03 +0200

    updated conferenceController

--- app/scripts/controllers/conferenceController.js
+++ app/scripts/controllers/conferenceController.js
@@ -1,32 +1,32 @@
 'use strict';
 /**
  * @ngdoc function
- * @name Quintet.controller:conferenceController
+ * @name sbAdminApp.controller:conferenceController
  * @description
- * Controller of the Quintet
+ * Controller of the sbAdminApp
  */
-angular.module('Quintet')
-    .controller('conferenceController', function ($state, $stateParams, $http, $scope, $rootScope, mainService, ConferenceService, uiCalendarConfig) {
+angular.module('sbAdminApp')
+    .controller('conferenceController', function ($state, $stateParams, $http, $scope, $rootScope, cfgService, ConferenceService, uiCalendarConfig) {
         // Event Source für Roadshows
         $scope.eventSource_roadshows = {
-            url: mainService.getControllerUrl() + "/conference/index?filter=roadshow&token=" + mainService.getToken(),
+            url: cfgService.getControllerUrl() + "/conference/index?category=roadshow&token=" + cfgService.getToken(),
             color: '#f7cc63',
             textColor: '#000000'
         };
 
         // Event Source für Conferences
         $scope.eventSource_conferences_scientific = {
-            url: mainService.getControllerUrl() + "/conference/index?filter=conference&subfilter=scientific&token=" + mainService.getToken(),
+            url: cfgService.getControllerUrl() + "/conference/index?category=conference&subcategory=scientific&token=" + cfgService.getToken(),
             color: '#FFbd50',
             textColor: '#000000'
         };
         $scope.eventSource_conferences_investor = {
-            url: mainService.getControllerUrl() + "/conference/index?filter=conference&subfilter=investor&token=" + mainService.getToken(),
+            url: cfgService.getControllerUrl() + "/conference/index?category=conference&subcategory=investor&token=" + cfgService.getToken(),
             color: '#52FF50',
             textColor: '#000000'
         };
         $scope.eventSource_conferences_other = {
-            url: mainService.getControllerUrl() + "/conference/index?filter=conference&subfilter=other&token=" + mainService.getToken(),
+            url: cfgService.getControllerUrl() + "/conference/index?category=conference&subcategory=other&token=" + cfgService.getToken(),
             color: '#52bdFF',
             textColor: '#000000'
         };
@@ -36,6 +36,8 @@ angular.module('Quintet')
         $scope.meetings_end = '';
         $scope.headline = "Conference Calendar";
         $scope.selected_Cal = $rootScope.selected_date;
+
+        //$scope.favorite_conferences = '';
 
         switch ($stateParams.mode) {
             case "roadshows":
@@ -52,6 +54,9 @@ angular.module('Quintet')
                     $scope.eventSource_conferences_other
                 ];
                 //console.log('eventSources_conferences = ', $scope.eventSources);
+                break;
+            case "favorite":
+                $scope.favorite_conferences = '';
                 break;
             default:
                 $scope.headline = "Error";
@@ -75,6 +80,30 @@ angular.module('Quintet')
             ConferenceService.getConferenceList(today, end_date, $scope.eventSource_roadshows.url, function(response) {
                 if(response) {
                     $scope.nextConferences = response;
+
+                    for(var i = 0; i < $scope.nextConferences.length; i++) {
+                        var start = $scope.nextConferences[i].start;
+                        var start_pos = 0, end_pos = 0;
+                        end_pos = start.indexOf('-', start_pos);
+                        var year = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        end_pos = start.indexOf('-', start_pos);
+                        var month = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        var day = start.slice(start_pos);
+                        $scope.nextConferences[i].start = month + '/' + day + '/' + year;
+
+                        var end = $scope.nextConferences[i].end;
+                        var start_pos = 0, end_pos = 0;
+                        end_pos = start.indexOf('-', start_pos);
+                        var year = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        end_pos = start.indexOf('-', start_pos);
+                        var month = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        var day = start.slice(start_pos);
+                        $scope.nextConferences[i].end = month + '/' + day + '/' + year;
+                    }
                 }
             });
 
@@ -82,6 +111,30 @@ angular.module('Quintet')
             ConferenceService.getConferenceList(start_date, today, $scope.eventSource_roadshows.url, function(response) {
                 if(response) {
                     $scope.pastConferences = response;
+
+                    for(var i = 0; i < $scope.pastConferences.length; i++) {
+                        var start = $scope.pastConferences[i].start;
+                        var start_pos = 0, end_pos = 0;
+                        end_pos = start.indexOf('-', start_pos);
+                        var year = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        end_pos = start.indexOf('-', start_pos);
+                        var month = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        var day = start.slice(start_pos);
+                        $scope.pastConferences[i].start = month + '/' + day + '/' + year;
+
+                        var end = $scope.pastConferences[i].end;
+                        var start_pos = 0, end_pos = 0;
+                        end_pos = start.indexOf('-', start_pos);
+                        var year = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        end_pos = start.indexOf('-', start_pos);
+                        var month = start.slice(start_pos, end_pos);
+                        start_pos = end_pos + 1;
+                        var day = start.slice(start_pos);
+                        $scope.pastConferences[i].end = month + '/' + day + '/' + year;
+                    }
                 }
             });
         }
@@ -127,5 +180,19 @@ angular.module('Quintet')
                 }
             }
         };
-        //console.log("conferenceController is running now... Done.");
+
+        $scope.detailPage = function(id) {
+            //console.log('detail page = ', id);
+            $state.go('quintet.conference_details', {id: id});
+        };
+
+        $scope.getFavoriteConferences = function() {
+            ConferenceService.getFavoriteConferences(function(response) {
+                if(response) {
+                    $scope.favorite_conferences = response;
+                }
+            });
+        };
+
+        $scope.getFavoriteConferences();
     });
