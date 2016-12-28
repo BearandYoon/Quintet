b0620c0022dfdc6257c30f92f77103b8fafa31ca
mac <luckysevenbear@gmail.com>
2016-12-28 11:59:18 +0200

    updated contactDetailsController

--- app/scripts/controllers/contactDetailsController.js
+++ app/scripts/controllers/contactDetailsController.js
@@ -1,114 +1,138 @@
 'use strict';
 /**
  * @ngdoc function
- * @name Quintet.controller:contactDetailsController
+ * @name sbAdminApp.controller:contactDetailsController
  * @description
  * # MainCtrl
- * Controller of the Quintet
+ * Controller of the sbAdminApp
  */
-angular.module('Quintet')
-        .controller('contactDetailsController', function (mainService, $scope, $state, $http, $stateParams) {
+angular.module('sbAdminApp')
+    .controller('contactDetailsController', function (cfgService, $scope, $state, $http, $stateParams, $uibModal, ContactService) {
+        console.log("stateParams: " + $stateParams);
 
-            console.log("stateParams: " + $stateParams);
+        $scope.id = $stateParams.id;
+        $scope.contact = {};
+        $scope.meetings = [];
+        $scope.contact_lists = [];
 
-            $scope.id = $stateParams.id;
-            $scope.contact = {};
-            $scope.meetings = [];
-            $scope.contact_lists = []
-
-            /**
-             * Loads all contact lists
-             * @returns {undefined}
-             */
-            $scope.loadContactLists = function () {
-                var promise = $http({
-                    method: "GET",
-                    cache: false,
-                    url: mainService.getControllerUrl() + "/contact_list/by_contact",
-                    params: {
-                        token: mainService.getToken(),
-                        contact_id: $stateParams.id,
-                    }
-                }).then(function (resp) {
-                    console.log("RESPONSE: " + resp);
-                    console.log(resp.data);
-                    $scope.contact_lists = resp.data;
-                }, function (resp) {
-                    mainService.httpErrorCallback(resp);
-                });
-            };
-
-            /**
-             * Changes the subscription of a the loaded contact
-             * @param {type} operation
-             * @param {type} contact_list_id
-             * @returns {undefined}
-             */
-            $scope.changeSubscription = function (operation, contact_list_id) {
-                console.log("Now changing...");
-
-                var promise = $http({
-                    method: "POST",
-                    cache: false,
-                    url: mainService.getControllerUrl() + "/contact_list/change",
-                    data: {
-                        contact_id: $stateParams.id,
-                        contact_list_id: contact_list_id,
-                        token: mainService.getToken(),
-                        operation: operation,
-                    }
-                }).then(function (resp) {
-                    console.log("RESPONSE: " + resp.data);
-                    if (resp.data.indexOf("ERROR") == -1) {
-                        alert(resp.data.replace("OK ", ""));
-                        $scope.loadContactLists();
-                    } else {
-                        alert(resp.data.replace("ERROR ", ""));
-                    }
-                }, function (resp) {
-                    mainService.httpErrorCallback(resp);
-                });
-            };
-
-
-            console.log("contactDetailsController is running now...");
-
-            mainService.showSpinner("Loading Contact...");
-            var promise_data = $http({
-                method: "GET",
-                cache: false,
-                url: mainService.getControllerUrl() + "/contact/index",
-                params: {
-                    id: $stateParams.id,
-                    token: mainService.getToken()
-                }
-            }).then(function (resp) {
-                console.log("RESPONSE:");
-                console.log(resp.data);
-                $scope.contact = resp.data;
-            }, function (resp) {
-                mainService.httpErrorCallback(resp);
-            }).finally(function() {
-                mainService.hideSpinner();
-            });
-
+        /**
+         * Loads all contact lists
+         * @returns {undefined}
+         */
+        $scope.loadContactLists = function () {
             var promise = $http({
                 method: "GET",
                 cache: false,
-                url: mainService.getControllerUrl() + "/meeting",
+                url: cfgService.getControllerUrl() + "/contact_list/by_contact",
                 params: {
-                    token: mainService.getToken(),
-                    contact_id: $stateParams.id,
+                    token: cfgService.getToken(),
+                    contact_id: $stateParams.id
                 }
             }).then(function (resp) {
-                console.log("RESPONSE: " + resp);
-                console.log(resp.data);
-                $scope.meetings = resp.data;
+                $scope.contact_lists = resp.data;
             }, function (resp) {
-                mainService.httpErrorCallback(resp);
+                cfgService.httpErrorCallback(resp);
             });
+        };
 
-            $scope.loadContactLists();
+        /**
+         * Changes the subscription of a the loaded contact
+         * @param {type} operation
+         * @param {type} contact_list_id
+         * @returns {undefined}
+         */
+        $scope.changeSubscription = function (operation, contact_list_id) {
+            var promise = $http({
+                method: "POST",
+                cache: false,
+                url: cfgService.getControllerUrl() + "/contact_list/change",
+                data: {
+                    contact_id: $stateParams.id,
+                    contact_list_id: contact_list_id,
+                    token: cfgService.getToken(),
+                    operation: operation
+                }
+            }).then(function (resp) {
+                if (resp.data.indexOf("ERROR") == -1) {
+                    alert(resp.data.replace("OK ", ""));
+                    $scope.loadContactLists();
+                } else {
+                    alert(resp.data.replace("ERROR ", ""));
+                }
+            }, function (resp) {
+                cfgService.httpErrorCallback(resp);
+            });
+        };
 
-            console.log("contactDetailsController is running now... Done.");
+        cfgService.showSpinner("Loading Contact...");
+        var promise_data = $http({
+            method: "GET",
+            cache: false,
+            url: cfgService.getControllerUrl() + "/contact/index",
+            params: {
+                id: $stateParams.id,
+                token: cfgService.getToken()
+            }
+        }).then(function (resp) {
+            $scope.contact = resp.data;
+        }, function (resp) {
+            cfgService.httpErrorCallback(resp);
+        }).finally(function() {
+            cfgService.hideSpinner();
         });
+
+        var promise = $http({
+            method: "GET",
+            cache: false,
+            url: cfgService.getControllerUrl() + "/meeting",
+            params: {
+                token: cfgService.getToken(),
+                contact_id: $stateParams.id
+            }
+        }).then(function (resp) {
+            $scope.meetings = resp.data;
+        }, function (resp) {
+            cfgService.httpErrorCallback(resp);
+        });
+
+        $scope.loadContactLists();
+
+        $scope.openEmailModal = function(isIntroduction, name) {
+            var modal = $uibModal.open({
+                animation: true,
+                templateUrl: 'views/email/sendEmail-modal.html',
+                controller: 'sendEmailController',
+                controllerAs: '$ctrl',
+                size: 'medium-st',
+                keyboard  : false,
+                resolve: {
+                    param: function () {
+                        return {
+                            'isAccount': false,
+                            'id' : $scope.id,
+                            'introduction': isIntroduction,
+                            'sel_contact_name' : name
+                        };
+                    }
+                }
+            });
+        };
+
+        $scope.favoriteContact = function() {
+            $scope.contact.is_favorite = !$scope.contact.is_favorite;
+
+            if($scope.contact.is_favorite) {
+                ContactService.setFavorite($stateParams.id, function(response) {
+                    if(!response) {
+                        $scope.contact.is_favorite = !$scope.contact.is_favorite;
+                    }
+                })
+            } else {
+                ContactService.removeFavorite($stateParams.id, function(response) {
+                    if(!response) {
+                        $scope.contact.is_favorite = !$scope.contact.is_favorite;
+                    }
+                })
+            }
+        };
+    });
